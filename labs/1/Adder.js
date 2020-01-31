1186f30a9c9a309c3d3cc965f216cc39e19aa0e3
Mekalin Paengwan <mekalinpaengwan@knights.ucf.edu>
2020-01-31 16:17:29 -0500

    lab1

---
+++ labs/1/Adder.js
@@ -0,0 +1,13 @@
+class Adder {
+    constructor(props){
+        this.props = props;
+            }
+            sum() {
+        return this.props.a + this.props.b;
+            }
+            render() {
+                return `<p>The sum of ${this.props.a} and ${this.props.b} is 5</p>`;
+            }
+        }
+
+module.exports = Adder; 
---
+++ labs/1/index.js
@@ -0,0 +1,10 @@
+var Adder = require("./Adder.js");
+
+    var input = {
+        a: 2,
+        b: 3,
+    };
+
+    var adderInstance = new Adder(input);
+
+console.log(adderInstance.render()); 
