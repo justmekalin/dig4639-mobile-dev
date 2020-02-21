aa0c99ec80e1689431233fbcf603a7508d9347ee
Mekalin Paengwan <mekalinpaengwan@knights.ucf.edu>
2020-02-20 19:47:09 -0500

    lab3

---
+++ labs/3/src/components/Card/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+//import css
+import './index.css';
+
+
+class Card extends React.Component {
+    render() {
+      return(
+        <div className = "card">
+            { this.props.content}
+  
+        </div>
+      )
+    }
+  }
+  export default Card
