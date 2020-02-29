8fdc67bd0f09070c4b51178609f58443cac6db61
Mekalin Paengwan <mekalinpaengwan@knights.ucf.edu>
2020-02-29 17:32:36 -0500

    changes

--- labs/4/src/App.js
+++ labs/4/src/App.js
@@ -3,12 +3,13 @@ import './App.css';
 import CardList from './components/CardList';
 
 
-class App extends React.Component{
-  render(){
+class App extends React.Component {
+  render() {
     return (
       <CardList/>
     )
   }
 }
+
 
 export default App;
--- labs/4/src/components/Card/index.js
+++ labs/4/src/components/Card/index.js
@@ -4,12 +4,11 @@ import './index.css';
 class Card extends React.Component {
     render() {
       return (
-        <div className="card">
-          <h3>{this.props.title}</h3>
-          <p>{this.props.content}</p>
-          <input type="button" value="x" className="close" //delete card
-            onClick={() => this.props.clickMe(this.props.id)} />
-        </div>
+        <div className = "card">
+        <span className="close" onClick={() => this.props.clickMe()}>&times;</span>
+        <h3>{this.props.title}</h3>
+        <p>{this.props.content}</p>
+      </div>
       );
     }
   }
