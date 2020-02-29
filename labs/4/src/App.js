87c090f73fbd805e942a0950d06e98e060bd1f69
Mekalin Paengwan <mekalinpaengwan@knights.ucf.edu>
2020-02-29 17:17:45 -0500

    lab4

---
+++ labs/4/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import CardList from './components/CardList';
+
+
+class App extends React.Component{
+  render(){
+    return (
+      <CardList/>
+    )
+  }
+}
+
+export default App;
